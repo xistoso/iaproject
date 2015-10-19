06fe40373c3b966db5c1193c7caf3d137941f27d
Pedro Miguel Casaleiro Dias <quitoso@gmail.com>
2015-10-19 17:15:17 +0100

    This closes #2 , closes #3 , closes #4 .

--- proj.lisp
+++ proj.lisp
@@ -1,3 +1,19 @@
-% 58534 Gil Gomes
-% 58540 Pedro Dias
-% 58551 Goncalo Ribeiro
+;; 58534 Gil Gomes
+;; 58540 Pedro Dias
+;; 58551 Goncalo Ribeiro
+
+;;2.1.1
+
+(defstruct accao (pos NIL)
+	   (arr NIL :type array))
+
+(defun cria-accao (pos arr)
+  (make-accao :pos pos :arr arr))
+
+(defun accao-coluna (acc)
+  (accao-pos acc))
+
+(defun accao-peca (acc)
+  (accao-arr acc))
+
+
