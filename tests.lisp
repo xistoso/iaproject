1fca82cedb5f5337fb8a141e195d7c6944ee27a9
Gil <gil@gilgomes.com>
2015-11-08 17:12:15 +0000

    added test 13 and 15 to tests.lisp

--- tests.lisp
+++ tests.lisp
@@ -62,3 +62,60 @@
 (print (concatenate 'string "linha preenchida:" (if (tabuleiro-linha-completa-p tabuleiro 3) "T" "F")))
 
 (print (concatenate 'string "linha preenchida:" (if (tabuleiro-linha-completa-p tabuleiro 7) "T" "F")))
+
+
+;;; Teste 13
+;;; Testes fn accoes
+
+;;deve retornar T
+(equalp '((0 . #2A((T) (T) (T) (T))) (1 . #2A((T) (T) (T) (T))) (2 . #2A((T) (T) (T) (T))) (3 . #2A((T) (T) (T) (T))) (4 . #2A((T) (T) (T) (T))) (5 . #2A((T) (T) (T) (T))) (6 . #2A((T) (T) (T) (T))) (7 . #2A((T) (T) (T) (T))) (8 . #2A((T) (T) (T) (T))) (9 . #2A((T) (T) (T) (T))) (0 . #2A((T T T T))) (1 . #2A((T T T T))) (2 . #2A((T T T T))) (3 . #2A((T T T T))) (4 . #2A((T T T T))) (5 . #2A((T T T T))) (6 . #2A((T T T T)))) (accoes (make-estado :pontos 50 :pecas-por-colocar '(i) :pecas-colocadas '(z z z) :tabuleiro (cria-tabuleiro))))
+
+;;deve retornar T
+(equalp '((0 . #2A((T T) (T NIL) (T NIL))) (1 . #2A((T T) (T NIL) (T NIL))) (2 . #2A((T T) (T NIL) (T NIL))) (3 . #2A((T T) (T NIL) (T NIL))) (4 . #2A((T T) (T NIL) (T NIL)))(5 . #2A((T T) (T NIL) (T NIL))) (6 . #2A((T T) (T NIL) (T NIL))) (7 . #2A((T T) (T NIL) (T NIL))) (8 . #2A((T T) (T NIL) (T NIL))) (0 . #2A((T NIL NIL) (T T T)))(1 . #2A((T NIL NIL) (T T T))) (2 . #2A((T NIL NIL) (T T T))) (3 . #2A((T NIL NIL) (T T T))) (4 . #2A((T NIL NIL) (T T T))) (5 . #2A((T NIL NIL) (T T T)))(6 . #2A((T NIL NIL) (T T T))) (7 . #2A((T NIL NIL) (T T T))) (0 . #2A((NIL T) (NIL T) (T T))) (1 . #2A((NIL T) (NIL T) (T T))) (2 . #2A((NIL T) (NIL T) (T T)))(3 . #2A((NIL T) (NIL T) (T T))) (4 . #2A((NIL T) (NIL T) (T T))) (5 . #2A((NIL T) (NIL T) (T T))) (6 . #2A((NIL T) (NIL T) (T T))) (7 . #2A((NIL T) (NIL T) (T T)))(8 . #2A((NIL T) (NIL T) (T T))) (0 . #2A((T T T) (NIL NIL T))) (1 . #2A((T T T) (NIL NIL T))) (2 . #2A((T T T) (NIL NIL T))) (3 . #2A((T T T) (NIL NIL T)))(4 . #2A((T T T) (NIL NIL T))) (5 . #2A((T T T) (NIL NIL T))) (6 . #2A((T T T) (NIL NIL T))) (7 . #2A((T T T) (NIL NIL T)))) (accoes (make-estado :pontos 50 :pecas-por-colocar '(l) :pecas-colocadas '(z z z) :tabuleiro (cria-tabuleiro))))
+
+;;deve retornar T
+(equalp '((0 . #2A((T T) (NIL T) (NIL T))) (1 . #2A((T T) (NIL T) (NIL T))) (2 . #2A((T T) (NIL T) (NIL T))) (3 . #2A((T T) (NIL T) (NIL T))) (4 . #2A((T T) (NIL T) (NIL T)))(5 . #2A((T T) (NIL T) (NIL T))) (6 . #2A((T T) (NIL T) (NIL T))) (7 . #2A((T T) (NIL T) (NIL T))) (8 . #2A((T T) (NIL T) (NIL T))) (0 . #2A((T T T) (T NIL NIL)))(1 . #2A((T T T) (T NIL NIL))) (2 . #2A((T T T) (T NIL NIL))) (3 . #2A((T T T) (T NIL NIL))) (4 . #2A((T T T) (T NIL NIL))) (5 . #2A((T T T) (T NIL NIL)))(6 . #2A((T T T) (T NIL NIL))) (7 . #2A((T T T) (T NIL NIL))) (0 . #2A((T NIL) (T NIL) (T T))) (1 . #2A((T NIL) (T NIL) (T T))) (2 . #2A((T NIL) (T NIL) (T T)))(3 . #2A((T NIL) (T NIL) (T T))) (4 . #2A((T NIL) (T NIL) (T T))) (5 . #2A((T NIL) (T NIL) (T T))) (6 . #2A((T NIL) (T NIL) (T T))) (7 . #2A((T NIL) (T NIL) (T T)))(8 . #2A((T NIL) (T NIL) (T T))) (0 . #2A((NIL NIL T) (T T T))) (1 . #2A((NIL NIL T) (T T T))) (2 . #2A((NIL NIL T) (T T T))) (3 . #2A((NIL NIL T) (T T T)))(4 . #2A((NIL NIL T) (T T T))) (5 . #2A((NIL NIL T) (T T T))) (6 . #2A((NIL NIL T) (T T T))) (7 . #2A((NIL NIL T) (T T T)))) (accoes (make-estado :pontos 50 :pecas-por-colocar '(j) :pecas-colocadas '(z z z) :tabuleiro (cria-tabuleiro))))
+
+;;deve retornar T
+(equalp '((0 . #2A((T T) (T T))) (1 . #2A((T T) (T T))) (2 . #2A((T T) (T T))) (3 . #2A((T T) (T T))) (4 . #2A((T T) (T T))) (5 . #2A((T T) (T T))) (6 . #2A((T T) (T T)))(7 . #2A((T T) (T T))) (8 . #2A((T T) (T T)))) (accoes (make-estado :pontos 50 :pecas-por-colocar '(o) :pecas-colocadas '(z z z) :tabuleiro (cria-tabuleiro))))
+
+;;deve retornar T
+(equalp '((0 . #2A((T T NIL) (NIL T T))) (1 . #2A((T T NIL) (NIL T T))) (2 . #2A((T T NIL) (NIL T T))) (3 . #2A((T T NIL) (NIL T T))) (4 . #2A((T T NIL) (NIL T T)))(5 . #2A((T T NIL) (NIL T T))) (6 . #2A((T T NIL) (NIL T T))) (7 . #2A((T T NIL) (NIL T T))) (0 . #2A((NIL T) (T T) (T NIL))) (1 . #2A((NIL T) (T T) (T NIL)))(2 . #2A((NIL T) (T T) (T NIL))) (3 . #2A((NIL T) (T T) (T NIL))) (4 . #2A((NIL T) (T T) (T NIL))) (5 . #2A((NIL T) (T T) (T NIL))) (6 . #2A((NIL T) (T T) (T NIL)))(7 . #2A((NIL T) (T T) (T NIL))) (8 . #2A((NIL T) (T T) (T NIL)))) (accoes (make-estado :pontos 50 :pecas-por-colocar '(s) :pecas-colocadas '(z z z) :tabuleiro (cria-tabuleiro))))
+
+;;deve retornar T
+(equalp '((0 . #2A((NIL T T) (T T NIL))) (1 . #2A((NIL T T) (T T NIL))) (2 . #2A((NIL T T) (T T NIL))) (3 . #2A((NIL T T) (T T NIL))) (4 . #2A((NIL T T) (T T NIL)))(5 . #2A((NIL T T) (T T NIL))) (6 . #2A((NIL T T) (T T NIL))) (7 . #2A((NIL T T) (T T NIL))) (0 . #2A((T NIL) (T T) (NIL T))) (1 . #2A((T NIL) (T T) (NIL T)))(2 . #2A((T NIL) (T T) (NIL T))) (3 . #2A((T NIL) (T T) (NIL T))) (4 . #2A((T NIL) (T T) (NIL T))) (5 . #2A((T NIL) (T T) (NIL T))) (6 . #2A((T NIL) (T T) (NIL T)))(7 . #2A((T NIL) (T T) (NIL T))) (8 . #2A((T NIL) (T T) (NIL T)))) (accoes (make-estado :pontos 50 :pecas-por-colocar '(z) :pecas-colocadas '(z z z) :tabuleiro (cria-tabuleiro))))
+
+;;deve retornar T
+(equalp '((0 . #2A((T T T) (NIL T NIL))) (1 . #2A((T T T) (NIL T NIL))) (2 . #2A((T T T) (NIL T NIL))) (3 . #2A((T T T) (NIL T NIL))) (4 . #2A((T T T) (NIL T NIL)))(5 . #2A((T T T) (NIL T NIL))) (6 . #2A((T T T) (NIL T NIL))) (7 . #2A((T T T) (NIL T NIL))) (0 . #2A((T NIL) (T T) (T NIL))) (1 . #2A((T NIL) (T T) (T NIL)))(2 . #2A((T NIL) (T T) (T NIL))) (3 . #2A((T NIL) (T T) (T NIL))) (4 . #2A((T NIL) (T T) (T NIL))) (5 . #2A((T NIL) (T T) (T NIL))) (6 . #2A((T NIL) (T T) (T NIL)))(7 . #2A((T NIL) (T T) (T NIL))) (8 . #2A((T NIL) (T T) (T NIL))) (0 . #2A((NIL T NIL) (T T T))) (1 . #2A((NIL T NIL) (T T T))) (2 . #2A((NIL T NIL) (T T T)))(3 . #2A((NIL T NIL) (T T T))) (4 . #2A((NIL T NIL) (T T T))) (5 . #2A((NIL T NIL) (T T T))) (6 . #2A((NIL T NIL) (T T T))) (7 . #2A((NIL T NIL) (T T T)))(0 . #2A((NIL T) (T T) (NIL T))) (1 . #2A((NIL T) (T T) (NIL T))) (2 . #2A((NIL T) (T T) (NIL T))) (3 . #2A((NIL T) (T T) (NIL T))) (4 . #2A((NIL T) (T T) (NIL T)))(5 . #2A((NIL T) (T T) (NIL T))) (6 . #2A((NIL T) (T T) (NIL T))) (7 . #2A((NIL T) (T T) (NIL T))) (8 . #2A((NIL T) (T T) (NIL T)))) (accoes (make-estado :pontos 50 :pecas-por-colocar '(t) :pecas-colocadas '(z z z) :tabuleiro (cria-tabuleiro))))
+
+
+
+;;; Teste 15
+;;; Testes fn resultado
+
+;;deve retornar IGNORE
+(ignore-value (setf estado1 (make-estado :pontos 0 :pecas-por-colocar '(o o o o o j) :pecas-colocadas '() :tabuleiro (cria-tabuleiro))))
+
+;;deve retornar IGNORE
+(ignore-value (setf estado2 (resultado estado1 '(0 . #2A((T T)(T T))))))
+
+;;deve retornar IGNORE
+(ignore-value (setf estado2 (resultado estado2 '(2 . #2A((T T)(T T))))))
+
+;;deve retornar IGNORE
+(ignore-value (setf estado2 (resultado estado2 '(4 . #2A((T T)(T T))))))
+
+;;deve retornar IGNORE
+(ignore-value (setf estado2 (resultado estado2 '(6 . #2A((T T)(T T))))))
+
+;;deve retornar T
+(tabuleiro-preenchido-p (estado-tabuleiro estado2) 1 5)
+
+;;deve retornar IGNORE
+(ignore-value (setf estado2 (resultado estado2 '(8 . #2A((T T)(T T))))))
+
+;;deve retornar 300
+(estado-pontos estado2)
+
+;;deve retornar NIL
+(tabuleiro-preenchido-p (estado-tabuleiro estado2) 1 5)
